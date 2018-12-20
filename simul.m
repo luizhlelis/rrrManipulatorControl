2354b5dd57198a89b1b71c0e957bdf3ff0b10845
Luiz Lelis <luizhlelis@gmail.com>
2018-12-20 01:24:24 -0200

    dh - matrizes homogeneas

--- simul.m
+++ simul.m
@@ -2,83 +2,62 @@ clc;
 clear all;
 close all;
 
-% tf1 = 2 polos, 1 zero, sem atraso
-% tf3 = 1 polo, 1 zero, sem atraso
+% ---------------------- DENAVIT - HATENBERG ------------------------
 
-mat = load('entradaSaida.mat','-mat');
+% Rodar o Matlab com um usuário com alto privilegio
 
-inOutMat = mat.entradaSaidaTemp;
-inMat = inOutMat(:,1);
-[inMatRows,inMatCols] = size(inMat)
-outMat = inOutMat(:,2);
+% 1) Carregando a matriz de Denavit-Hatenberg
+mat = load('matrix/DH.mat','-mat');
+dh = mat.dh
 
-% Condicoes iniciais iguais a zero
-filtIn = inMat(904:inMatRows)-20;
-filtOut = outMat(904:inMatRows)-36.29;
+dh_representation = SerialLink(dh)
 
-[inRows,inCols] = size(filtIn)
+% Carregando angulos iniciais das juntas (radianos)
+theta_0 = 0;
+theta_1 = 0;
+theta_2 = 0;
+q = [theta_0, theta_1, theta_2]
 
-% TF 1
-tf1Mat = load('tfs/tf1.mat','-mat');
-tf1 = tf1Mat.tf1;
-tf1 = tf(tf1.Numerator,tf1.Denominator)
-t = 0:1:(inRows-1);
-resp1 = lsim(tf1,filtIn,t);
+% Representacao grafica do manipulador
+dh_representation.plot(q)
+dh_representation.teach
 
-% % TF 2
-% tf2Mat = load('tfs/tf2.mat','-mat');
-% tf2 = tf2Mat.tf2;
-% tf2 = tf(tf2.Numerator,tf2.Denominator,'InputDelay',tf1.IODelay)
-% t = 0:1:(inMatRows-1);
-% resp2 = lsim(tf2,inMat,t);
-% 
-% % TF 3
-% tf3Mat = load('tfs/tf3.mat','-mat');
-% tf3 = tf3Mat.tf3;
-% tf3 = tf(tf3.Numerator,tf3.Denominator)
-% t = 0:1:(inMatRows-1);
-% resp3 = lsim(tf3,inMat,t);
+% 2) Matrizes homogeneas
 
-figure(1)
-plot(filtIn,'g')
-hold on
-plot(filtOut,'b')
-hold on
-plot(resp1,'r')
+% Especificidades do manipulador em estudo
+alpha_0 = -pi/2;
+alpha_1 = 0;
+alpha_2 = 0;
 
-%sisotool(tf1)
+a0 = 0.065;
+a1 = 0.172;
+a2 = 0.235;
 
-contPI = tf([33.731 33.731*0.009023],[1 0]);
-temp1 = series(contPI,tf1);
-temp2 = feedback(temp1,1);
-
-% contPI = tf([38.359 0.009023],[1 0]);
-% temp1 = series(contPI,tf1);
-% temp2 = feedback(temp1,1);
-
-temp3 = feedback(tf1,1);
-
-temp4 = feedback(contPI,tf1);
-
-figure(2)
-step(14*temp2)
-hold on
-step(14*temp3)
-
-figure(3)
-step(14*temp4)
+d0 = 0.085;
+d1 = 0;
+d2 = 0;
 
 
-% Atraso de fase
-%   33.3 (s+0.01147)
-%   ----------------
-%     (s+0.003774)
+A_0 =   [
+            [cos(theta_0)   -sin(theta_0)*cos(alpha_0)  sin(theta_0)*sin(alpha_0)   a0*cos(theta_0)];
+            [sin(theta_0)   cos(theta_0)*cos(alpha_0)   -cos(theta_0)*sin(alpha_0)  a0*sin(theta_0)];
+            [0              sin(alpha_0)                -cos(alpha_0)               d0             ];
+            [0              0                           0                           1              ];
+        ];
+A_1 =   [
+            [cos(theta_1)   -sin(theta_1)*cos(alpha_1)  sin(theta_1)*sin(alpha_1)   a1*cos(theta_1)];
+            [sin(theta_1)   cos(theta_1)*cos(alpha_1)   -cos(theta_1)*sin(alpha_1)  a1*sin(theta_1)];
+            [1              sin(alpha_1)                -cos(alpha_1)               d1             ];
+            [0              0                           0                           1              ];
+        ];
+A_2 =   [
+            [cos(theta_2)   -sin(theta_2)*cos(alpha_2)  sin(theta_2)*sin(alpha_2)   a2*cos(theta_2)];
+            [sin(theta_2)   cos(theta_2)*cos(alpha_2)   -cos(theta_2)*sin(alpha_2)  a2*sin(theta_2)];
+            [2              sin(alpha_2)                -cos(alpha_2)               d2             ];
+            [0              0                           0                           1              ];
+        ];
 
-% PI
-%   33.731 (s+0.009023)
-%   -------------------
-%            s
+% Matriz homogenea resultante
+A = A_0*A_1*A_2
 
-%   38.359 (s+0.01109)
-%   ------------------
-%           s
+% ----------------------- EULER - LAGRANGE -----------------------------
