e1db6733087df6085367d10565d603c58acd27b7
Luiz Lelis <luizhlelis@gmail.com>
2019-02-14 23:58:43 -0200

    simul pronta - controlador feito

--- simulated/simul.m
+++ simulated/simul.m
@@ -1,5 +1,4 @@
 clc;
-clear all;
 close all;
 
 load('base/input_base_01s.mat')
@@ -80,4 +79,51 @@ hold on
 plot(time_forearm(1,:),result_step_forearm,'g');
 axis([5 20 20 110])
 legend('degrau','real','simulado')
-title('Simulacao Ombro Manipulador')
+title('Simulacao Cotovelo Manipulador')
+
+% ------------------------ Malha Fechada ------------------------- %
+
+c_base = tf([3.762 3.42],[1 0])
+mf_base = feedback(c_base*tf_base,1)
+
+result_mf_step_base = lsim(mf_base,ref_base,time_base);
+
+figure(4)
+plot(time_base(1,:),ref_base(1,:),'b');
+hold on
+plot(output_base_01s(:,1),output_base_01s(:,2),'r');
+hold on
+plot(time_base(1,:),result_mf_step_base,'g');
+axis([5 18 180 300])
+legend('degrau','real','simulado')
+title('Simulacao MF Base Manipulador')
+
+c_shoulder = tf([2.2436 3.16],[1 0])
+mf_shoulder = feedback(c_shoulder*tf_shoulder,1)
+
+result_mf_step_shoulder = lsim(mf_shoulder,ref_shoulder,time_shoulder);
+
+figure(5)
+plot(time_shoulder(1,:),ref_shoulder(1,:),'b');
+hold on
+plot(output_shoulder_01s(:,1),output_shoulder_01s(:,2),'r');
+hold on
+plot(time_shoulder(1,:),result_mf_step_shoulder,'g');
+axis([5 20 30 100])
+legend('degrau','real','simulado')
+title('Simulacao MF Ombro Manipulador')
+
+c_forearm = tf([3.74 3.4],[1 0])
+mf_forearm = feedback(c_forearm*tf_forearm,1)
+
+result_mf_step_forearm = lsim(mf_forearm,ref_forearm,time_forearm);
+
+figure(6)
+plot(time_forearm(1,:),ref_forearm(1,:),'b');
+hold on
+plot(output_forearm_01s(:,1),output_forearm_01s(:,2),'r');
+hold on
+plot(time_forearm(1,:),result_mf_step_forearm,'g');
+axis([5 20 20 110])
+legend('degrau','real','simulado')
+title('Simulacao Cotovelo Manipulador')
