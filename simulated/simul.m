8fbff1023004bd53627388a7afa6498bce833426
Luciano Frezzatto <lucianofrezzatto@sala2504m06.cpdee.ufmg.br>
2019-02-28 16:50:46 -0300

    Adicionando stairs

--- simulated/simul.m
+++ simulated/simul.m
@@ -9,6 +9,7 @@ load('forearm/output_forearm_01s.mat')
 load('forearm/input_forearm_01s.mat')
 
 tf_base = tf([1.005],[1 1.006])
+tf_base = c2d(tf_base,0.01,'zoh')
 time_base = 0:0.01:19.99;
 ref_base = zeros(1,2000);
 
@@ -23,16 +24,17 @@ end
 result_step_base = lsim(tf_base,ref_base,time_base);
 
 figure(1)
-plot(time_base(1,:),ref_base(1,:),'b');
+stairs(time_base(1,:),ref_base(1,:),'b');
 hold on
-plot(output_base_01s(:,1),output_base_01s(:,2),'r');
+stairs(output_base_01s(:,1),output_base_01s(:,2),'r');
 hold on
-plot(time_base(1,:),result_step_base,'g');
+stairs(time_base(1,:),result_step_base,'g');
 axis([5 18 180 300])
 legend('degrau','real','simulado')
 title('Simulacao Base Manipulador')
 
 tf_shoulder = tf([1.583],[1 1.596])
+tf_shoulder = c2d(tf_shoulder,0.01,'zoh')
 time_shoulder = 0:0.01:19.99;
 ref_shoulder = zeros(1,2000);
 
@@ -47,17 +49,18 @@ end
 result_step_shoulder = lsim(tf_shoulder,ref_shoulder,time_shoulder);
 
 figure(2)
-plot(time_shoulder(1,:),ref_shoulder(1,:),'b');
+stairs(time_shoulder(1,:),ref_shoulder(1,:),'b');
 hold on
-plot(output_shoulder_01s(:,1),output_shoulder_01s(:,2),'r');
+stairs(output_shoulder_01s(:,1),output_shoulder_01s(:,2),'r');
 hold on
-plot(time_shoulder(1,:),result_step_shoulder,'g');
+stairs(time_shoulder(1,:),result_step_shoulder,'g');
 axis([5 20 30 100])
 legend('degrau','real','simulado')
 title('Simulacao Ombro Manipulador')
 
 
 tf_forearm = tf([0.9556],[1 0.9407])
+tf_forearm = c2d(tf_forearm,0.01,'zoh')
 time_forearm = 0:0.01:19.99;
 ref_forearm = zeros(1,2000);
 
@@ -72,11 +75,11 @@ end
 result_step_forearm = lsim(tf_forearm,ref_forearm,time_forearm);
 
 figure(3)
-plot(time_forearm(1,:),ref_forearm(1,:),'b');
+stairs(time_forearm(1,:),ref_forearm(1,:),'b');
 hold on
-plot(output_forearm_01s(:,1),output_forearm_01s(:,2),'r');
+stairs(output_forearm_01s(:,1),output_forearm_01s(:,2),'r');
 hold on
-plot(time_forearm(1,:),result_step_forearm,'g');
+stairs(time_forearm(1,:),result_step_forearm,'g');
 axis([5 20 20 110])
 legend('degrau','real','simulado')
 title('Simulacao Cotovelo Manipulador')
@@ -84,46 +87,49 @@ title('Simulacao Cotovelo Manipulador')
 % ------------------------ Malha Fechada ------------------------- %
 
 c_base = tf([3.762 3.42],[1 0])
+c_base = c2d(c_base,0.01,'zoh')
 mf_base = feedback(c_base*tf_base,1)
 
 result_mf_step_base = lsim(mf_base,ref_base,time_base);
 
 figure(4)
-plot(time_base(1,:),ref_base(1,:),'b');
+stairs(time_base(1,:),ref_base(1,:),'b');
 hold on
-plot(output_base_01s(:,1),output_base_01s(:,2),'r');
+stairs(output_base_01s(:,1),output_base_01s(:,2),'r');
 hold on
-plot(time_base(1,:),result_mf_step_base,'g');
+stairs(time_base(1,:),result_mf_step_base,'g');
 axis([5 18 180 300])
 legend('degrau','real','simulado')
 title('Simulacao MF Base Manipulador')
 
 c_shoulder = tf([2.2436 3.16],[1 0])
+c_shoulder = c2d(c_shoulder,0.01,'zoh')
 mf_shoulder = feedback(c_shoulder*tf_shoulder,1)
 
 result_mf_step_shoulder = lsim(mf_shoulder,ref_shoulder,time_shoulder);
 
 figure(5)
-plot(time_shoulder(1,:),ref_shoulder(1,:),'b');
+stairs(time_shoulder(1,:),ref_shoulder(1,:),'b');
 hold on
-plot(output_shoulder_01s(:,1),output_shoulder_01s(:,2),'r');
+stairs(output_shoulder_01s(:,1),output_shoulder_01s(:,2),'r');
 hold on
-plot(time_shoulder(1,:),result_mf_step_shoulder,'g');
+stairs(time_shoulder(1,:),result_mf_step_shoulder,'g');
 axis([5 20 30 100])
 legend('degrau','real','simulado')
 title('Simulacao MF Ombro Manipulador')
 
 c_forearm = tf([3.74 3.4],[1 0])
+c_forearm = c2d(c_forearm,0.01,'zoh')
 mf_forearm = feedback(c_forearm*tf_forearm,1)
 
 result_mf_step_forearm = lsim(mf_forearm,ref_forearm,time_forearm);
 
 figure(6)
-plot(time_forearm(1,:),ref_forearm(1,:),'b');
+stairs(time_forearm(1,:),ref_forearm(1,:),'b');
 hold on
-plot(output_forearm_01s(:,1),output_forearm_01s(:,2),'r');
+stairs(output_forearm_01s(:,1),output_forearm_01s(:,2),'r');
 hold on
-plot(time_forearm(1,:),result_mf_step_forearm,'g');
+stairs(time_forearm(1,:),result_mf_step_forearm,'g');
 axis([5 20 20 110])
 legend('degrau','real','simulado')
 title('Simulacao Cotovelo Manipulador')
